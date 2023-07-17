ab978ab38758c14596e40ba538d41180453582ac
한규진 <david0218@naver.com>
2023-07-17 19:02:19 +0900

    chore : 절대경로 세팅

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,11 @@ import svgr from 'vite-plugin-svgr';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), svgr()],
-})
+  resolve: {
+    alias: [
+      { find: '@', replacement: '/src' },
+      { find: '@pages', replacement: '/src/pages' },
+      { find: '@assets', replacement: '/src/assets' },
+    ],
+  },
+});
