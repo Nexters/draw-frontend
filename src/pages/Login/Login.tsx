b389c88ed13786ef12eeddbda623dcae924a4bca
Shim MunSeong <Puterism.K@gmail.com>
2023-07-18 22:54:46 +0900

    feat: 프로필 작성 페이지 UI 구현

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,15 +1,28 @@
+import { useNavigate } from 'react-router-dom';
 import Styled from './Login.styles';
 import LoginButton from './components/LoginButton/LoginButton';
 
 const Login = () => {
+  const navigate = useNavigate();
+
+  const handleClickKakaoLogin = () => {
+    // TODO: 실제 로그인 로직 구현 필요
+    navigate('/new-profile');
+  };
+
+  const handleClickAppleLogin = () => {
+    // TODO: 실제 로그인 로직 구현 필요
+    navigate('/new-profile');
+  };
+
   return (
     <Styled.Login>
       <Styled.GraphicContainer>
         <Styled.Graphic />
       </Styled.GraphicContainer>
       <Styled.LoginButtonContainer>
-        <LoginButton serviceName="kakao" />
-        <LoginButton serviceName="apple" />
+        <LoginButton serviceName="kakao" onClick={handleClickKakaoLogin} />
+        <LoginButton serviceName="apple" onClick={handleClickAppleLogin} />
       </Styled.LoginButtonContainer>
     </Styled.Login>
   );
--- src/pages/NewProfile/NewProfile.tsx
+++ src/pages/NewProfile/NewProfile.tsx
@@ -1,5 +1,99 @@
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import Styled from './NewProfile.styles';
+import BinaryPicker from '@/components/BinaryPicker/BinaryPicker';
+import { BIRTHDAY_REGEXP_STRING } from '@/constants/regexp';
+import MBTIPicker, { MBTI } from '@/components/MBTIPicker/MBTIPicker';
+
+const BIRTHDAY_LENGTH = 6;
+
 const NewProfile = () => {
-  return <>1-2. 프로필 작성</>;
+  const navigate = useNavigate();
+
+  const [gender, setGender] = useState<string | null>(null);
+  const [birthday, setBirthday] = useState<string>('');
+  const [mbti, setMBTI] = useState<MBTI>([null, null, null, null]);
+
+  const handleChangeGender = (value: string) => {
+    setGender(value);
+  };
+
+  const handleChangeBirthday = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setBirthday(event.target.value);
+  };
+
+  const handleChangeMBTI = (value: MBTI) => {
+    setMBTI(value);
+  };
+
+  const handleSubmitForm = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    // TODO: API 연동
+    navigate('/new-profile-card-view');
+  };
+
+  const isValidForm = gender !== null && birthday.length === BIRTHDAY_LENGTH && mbti.every((value) => value !== null);
+
+  return (
+    <Styled.NewProfile>
+      <Styled.ProfileForm onSubmit={handleSubmitForm}>
+        <Styled.ProfileFormSection>
+          <Styled.ProfileFormHeading>
+            성별과 나이를
+            <br />
+            알려주세요
+          </Styled.ProfileFormHeading>
+          <Styled.ProfileFormList>
+            <Styled.ProfileFormItem>
+              <Styled.ProfileFormLabel>성별</Styled.ProfileFormLabel>
+              <BinaryPicker
+                options={[
+                  {
+                    value: 'male',
+                    label: '남자',
+                  },
+                  {
+                    value: 'female',
+                    label: '여자',
+                  },
+                ]}
+                onChange={handleChangeGender}
+              />
+            </Styled.ProfileFormItem>
+            <Styled.ProfileFormItem>
+              <Styled.ProfileFormLabel>나이 (생년월일)</Styled.ProfileFormLabel>
+              <Styled.ProfileFormInput
+                type="text"
+                value={birthday}
+                placeholder="예) 980903"
+                minLength={BIRTHDAY_LENGTH}
+                maxLength={BIRTHDAY_LENGTH}
+                pattern={BIRTHDAY_REGEXP_STRING}
+                onChange={handleChangeBirthday}
+              />
+            </Styled.ProfileFormItem>
+          </Styled.ProfileFormList>
+        </Styled.ProfileFormSection>
+        <Styled.ProfileFormSection>
+          <Styled.ProfileFormHeading>
+            MBTI를
+            <br />
+            알려주세요
+          </Styled.ProfileFormHeading>
+          <Styled.ProfileFormItem>
+            <MBTIPicker onChange={handleChangeMBTI} />
+          </Styled.ProfileFormItem>
+        </Styled.ProfileFormSection>
+        <Styled.ProfileFormSubmit>
+          <Styled.ProfileFormSubmitMessage>온보딩 이후에 수정할 수 없어요</Styled.ProfileFormSubmitMessage>
+          <Styled.ProfileFormSubmitButton type="submit" disabled={!isValidForm}>
+            시작하기
+          </Styled.ProfileFormSubmitButton>
+        </Styled.ProfileFormSubmit>
+      </Styled.ProfileForm>
+    </Styled.NewProfile>
+  );
 };
 
 export default NewProfile;
