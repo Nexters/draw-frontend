3fbfe5244ada96bd68956dfd450e1247473b6ddf
한규진 <david0218@naver.com>
2023-07-24 14:40:40 +0900

    chore : 옵션선택 페이지 binary picker

--- src/pages/NewQuestion/components/OptionSelect/OptionSelect.styles.ts
+++ src/pages/NewQuestion/components/OptionSelect/OptionSelect.styles.ts
@@ -12,4 +12,37 @@ const OptionSelectWrapper = styled.div`
   z-index: 2;
 `;
 
-export default { OptionSelectWrapper };
+const PaddingContainer = styled.div`
+  padding: 0 24px;
+`;
+
+const Heading = styled.div`
+  margin-top: 28px;
+  ${({ theme }) => theme.typo['heading.1']};
+`;
+const Description = styled.div`
+  margin-top: 8px;
+  ${({ theme }) => theme.typo['sub.3']}
+  color : ${({ theme }) => theme.palette.text.grey1};
+`;
+
+const OptionFormContainer = styled.div``;
+const OptionFormHeading = styled.div`
+  ${({ theme }) => theme.typo['sub.3']}
+  color : ${({ theme }) => theme.palette.text.black};
+`;
+const OptionFormDescription = styled.div`
+  ${({ theme }) => theme.typo['sub.4.m']};
+  color: ${({ theme }) => theme.palette.text.grey2};
+  margin-top: 2px;
+`;
+
+export default {
+  OptionSelectWrapper,
+  Heading,
+  PaddingContainer,
+  Description,
+  OptionFormContainer,
+  OptionFormHeading,
+  OptionFormDescription,
+};
--- src/pages/NewQuestion/components/OptionSelect/OptionSelect.tsx
+++ src/pages/NewQuestion/components/OptionSelect/OptionSelect.tsx
@@ -1,16 +1,89 @@
 import TopBar from '@/components/TopBar/TopBar';
 import Styled from './OptionSelect.styles';
 import { animated } from '@react-spring/web';
+import Spacing from '@/components/Spacing/Spacing';
+import BinaryPicker from '@/components/BinaryPicker/BinaryPicker';
+import MBTIPicker, { MBTI } from '@/components/MBTIPicker/MBTIPicker';
+import { useState } from 'react';
 
 interface OptionSelectProps {
   closeOptionSelect: () => void;
 }
 
 const OptionSelect = animated(({ closeOptionSelect, ...rest }: OptionSelectProps) => {
+  const [gender, setGender] = useState<string | null>(null);
+  const [isPeer, setIsPeer] = useState<string | null>('all');
+  const [mbti, setMBTI] = useState<MBTI>([null, null, null, null]);
+
+  const handleChangeGender = (value: string) => {
+    setGender(value);
+  };
+  const handleChangeIsPeer = (value: string) => {
+    setIsPeer(value);
+  };
+  const handleChangeMBTI = (value: MBTI) => {
+    setMBTI(value);
+  };
+
   return (
-    <Styled.OptionSelectWrapper {...rest}>
-      <TopBar rightElement={'확인'} onClickBack={closeOptionSelect} />
-    </Styled.OptionSelectWrapper>
+    <>
+      <Styled.OptionSelectWrapper {...rest}>
+        <TopBar rightElement={'확인'} onClickBack={closeOptionSelect} />
+        <Styled.PaddingContainer>
+          <Styled.Heading>
+            누구한테 질문하고
+            <br />
+            싶으신가요?
+          </Styled.Heading>
+          <Styled.Description>원하는 답변자가 있다면 선택해 주세요</Styled.Description>
+          <Spacing size={50} />
+          <Styled.OptionFormContainer>
+            <Styled.OptionFormHeading>성별</Styled.OptionFormHeading>
+            <Spacing size={16} />
+            <BinaryPicker
+              options={[
+                {
+                  value: 'male',
+                  label: '남자',
+                },
+                {
+                  value: 'female',
+                  label: '여자',
+                },
+              ]}
+              onChange={handleChangeGender}
+            />
+          </Styled.OptionFormContainer>
+          <Spacing size={32} />
+          <Styled.OptionFormContainer>
+            <Styled.OptionFormHeading>나이</Styled.OptionFormHeading>
+            <Styled.OptionFormDescription>또래는 같은 ‘나이대’입니다.</Styled.OptionFormDescription>
+            <Spacing size={12} />
+            <BinaryPicker
+              options={[
+                {
+                  value: 'all',
+                  label: '전체',
+                },
+                {
+                  value: 'peer',
+                  label: '또래',
+                },
+              ]}
+              onChange={handleChangeIsPeer}
+              defaultChecked="peer"
+            />
+          </Styled.OptionFormContainer>
+          <Spacing size={41} />
+          <Styled.OptionFormContainer>
+            <Styled.OptionFormHeading>MBTI</Styled.OptionFormHeading>
+            <Styled.OptionFormDescription>최대 4개까지 선택할 수 있어요.</Styled.OptionFormDescription>
+            <Spacing size={14} />
+            <MBTIPicker onChange={handleChangeMBTI} />
+          </Styled.OptionFormContainer>
+        </Styled.PaddingContainer>
+      </Styled.OptionSelectWrapper>
+    </>
   );
 });
 
