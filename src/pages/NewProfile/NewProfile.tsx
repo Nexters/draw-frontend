cc29d8e3ab534df9488ef5e90154bca9592c8288
한규진 <david0218@naver.com>
2023-07-27 19:41:50 +0900

    chore : 사용하지 않는 import 삭제

--- src/pages/NewProfile/NewProfile.tsx
+++ src/pages/NewProfile/NewProfile.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import Styled from './NewProfile.styles';
 import BinaryPicker from '@/components/BinaryPicker/BinaryPicker';
