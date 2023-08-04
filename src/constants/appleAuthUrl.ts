ebde081ee0c5ecc4352dc5be14af03402cf74b3d
한규진 <david0218@naver.com>
2023-08-04 18:32:07 +0900

    chore : 애플로그인 responsemode query

--- src/constants/appleAuthUrl.ts
+++ src/constants/appleAuthUrl.ts
@@ -3,10 +3,9 @@ const APPLE_LOGIN_REDIRECT_URL = `${import.meta.env.VITE_DOMAIN}/callback/apple`
 const config = {
   client_id: APPLE_LOGIN_CLIENT_ID, // This is the service ID we created.
   redirect_uri: APPLE_LOGIN_REDIRECT_URL, // As registered along with our service ID
-  response_type: 'code id_token',
-  scope: 'name', // To tell apple we want the user name and emails fields in the response it sends us.
-  response_mode: 'form_post',
-  usePopup: true,
+  response_type: 'code',
+  //scope: 'name', // To tell apple we want the user name and emails fields in the response it sends us.
+  response_mode: 'query',
   // m: 11,
   // v: '1.5.4',
   //nonce: import.meta.env.VITE_APPLE_NONCE,
