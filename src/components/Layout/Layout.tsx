df9c6b56060b3e7edc39c50f115c820b2b7a2a0f
Shim MunSeong <puterism.k@gmail.com>
2023-08-05 12:51:06 +0900

    feat: 웹뷰를 감지하여 상하단 여백을 표시하도록 구현 (#36)
    * feat: 웹뷰를 감지하여 상하단 여백을 표시하도록 구현
    
    * fix: hasTabBar 조건식 수정

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,5 +1,6 @@
 import { useLayoutEffect } from 'react';
 import { Global, css } from '@emotion/react';
+import isUserAgentWebview from 'is-ua-webview';
 import { palette } from '@/styles/palette';
 import Styled from './Layout.styles';
 import useNativeMessage from '@/hooks/useNativeMessage';
@@ -15,12 +16,14 @@ const Layout = ({ backgroundColor, hasTabBar, children }: LayoutProps) => {
   const { showBottomBar } = useNativeMessage();
   useNavigateEvent();
 
+  const isWebview = isUserAgentWebview(window.navigator.userAgent);
+
   useLayoutEffect(() => {
     showBottomBar(hasTabBar ?? false);
   }, [hasTabBar, showBottomBar]);
 
   return (
-    <Styled.Layout hasTabBar={hasTabBar}>
+    <Styled.Layout hasNotchPadding={isWebview} hasTabBar={isWebview ? hasTabBar : isWebview}>
       <Global
         styles={css`
           body {
