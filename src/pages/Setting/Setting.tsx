e92aec90f811f52964eab16eb7a335c7c11ad34b
한규진 <55226431+9yujin@users.noreply.github.com>
2023-08-13 07:18:00 +0900

    feat : 임시 회원탈퇴 (#64)

--- src/pages/Setting/Setting.tsx
+++ src/pages/Setting/Setting.tsx
@@ -3,9 +3,17 @@ import Styled from './Setting.styles';
 import { ReactComponent as BackArrowIcon } from '@/assets/back-arrow.svg';
 import { palette } from '@/styles/palette';
 import Layout from '@/components/Layout/Layout';
+import useToast from '@/hooks/useToast';
 
 const Setting = () => {
   const navigate = useNavigate();
+  const toast = useToast();
+  const handleWithdrawUser = () => {
+    window.localStorage.removeItem('aT');
+    window.localStorage.removeItem('rT');
+    toast.success(<>회원탈퇴를 성공했어요</>);
+    navigate('/login');
+  };
 
   return (
     <Layout backgroundColor={palette.background.white1} hasTabBar>
@@ -16,7 +24,9 @@ const Setting = () => {
       </Styled.Header>
       <Styled.SettingList>
         <Styled.SettingButton type="button">이용약관</Styled.SettingButton>
-        <Styled.SettingButton type="button">회원 탈퇴</Styled.SettingButton>
+        <Styled.SettingButton type="button" onClick={handleWithdrawUser}>
+          회원 탈퇴
+        </Styled.SettingButton>
       </Styled.SettingList>
     </Layout>
   );
