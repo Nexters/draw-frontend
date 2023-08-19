3f6fec2978e6aaad28fd6e1466722e41ad986cc0
한규진 <david0218@naver.com>
2023-08-19 15:20:25 +0900

    fix : 한번 뒤집은카드 상태 업데이트

--- src/pages/QuestionDetail/components/AnswerCard/AnswerCard.tsx
+++ src/pages/QuestionDetail/components/AnswerCard/AnswerCard.tsx
@@ -2,18 +2,25 @@ import { useEffect, useRef, useState } from 'react';
 import Styled from './AnswerCard.styles';
 import CardFace from './CardFace';
 import { ReplyResponse } from '@/apis/types/feed';
-import { useMutation } from '@tanstack/react-query';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { replyApi } from '@/apis/handlers/reply';
 import { ReplyWriterRes } from '@/apis/types/reply';
+import { useParams } from 'react-router-dom';
 
 const AnswerCard = ({ replyData, onSelectAnswer }: { replyData: ReplyResponse; onSelectAnswer: () => void }) => {
   const { id, writer, content, status } = replyData;
+  const queryClient = useQueryClient();
+  const { id: feedId } = useParams<{ id: string }>();
+
   const ref = useRef<any>();
   const [isFlipped, setIsFlipped] = useState(false);
   const [nowPeekedWriterInfo, setNowPeekedWriterInfo] = useState<ReplyWriterRes | null>(null);
 
   const { mutate: postPeekMutate } = useMutation(replyApi.postPeek, {
-    onSuccess: (data) => setNowPeekedWriterInfo(data),
+    onSuccess: async (data) => {
+      setNowPeekedWriterInfo(data);
+      await queryClient.invalidateQueries(['feed-replies', feedId]);
+    },
   });
   useEffect(() => {
     nowPeekedWriterInfo && setIsFlipped(true);
